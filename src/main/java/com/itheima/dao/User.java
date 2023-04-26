1bbeab0df4b5d89602c3e67a0a5a096a6e308320
gallantsa <gallantsa@163.com>
2023-04-26 11:34:01 +0800

    测试克隆2

--- src/main/java/com/itheima/dao/User.java
+++ src/main/java/com/itheima/dao/User.java
@@ -5,4 +5,20 @@ package com.itheima.dao;
  * @version 1.0
  */
 public class User {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
